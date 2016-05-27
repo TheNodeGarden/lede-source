64873e89de413d8c8b9f54b54e0bf119f3fb7ec4
David Thornley <thornley.david@nodegarden.com>
2016-05-28 00:04:23 +1000

    * Cleaned up tabbing.

--- target/linux/ar71xx/generic/profiles/zsun.mk
+++ target/linux/ar71xx/generic/profiles/zsun.mk
@@ -4,14 +4,13 @@
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
 #
-
 define Profile/ZSUNSDREADER
-        NAME:=ZSUN WiFi SD Card reader
-        PACKAGES:=kmod-usb-core kmod-usb2 kmod-usb-storage
+	NAME:=ZSUN WiFi SD Card reader
+	PACKAGES:=kmod-usb-core kmod-usb2 kmod-usb-storage
 endef
 
 define Profile/ZSUNSDREADER/Description
-        Package set optimized for the ZSUN WiFi SD Card Reader.
+	Package set optimized for the ZSUN WiFi SD Card Reader.
 endef
 
 $(eval $(call Profile,ZSUNSDREADER))
